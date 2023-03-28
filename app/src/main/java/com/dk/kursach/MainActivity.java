5a95d0d89761ecec8756cca6f7134eaa286d2074
Danil_DESKTOP <vladimirovichdanil@bk.ru>
2023-03-28 13:25:55 +0300

    add firebase remote config

--- app/src/main/java/com/dk/kursach/MainActivity.java
+++ app/src/main/java/com/dk/kursach/MainActivity.java
@@ -1,14 +1,48 @@
 package com.dk.kursach;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.BuildConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 public class MainActivity extends AppCompatActivity {
-
+    FirebaseRemoteConfig rem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rem = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setMinimumFetchIntervalInSeconds(3600)
+                .build();
+        rem.setConfigSettingsAsync(configSettings);
+        rem.fetchAndActivate().addOnCompleteListener(new OnCompleteListener<Boolean>() {
+            @Override
+            public void onComplete(@NonNull Task<Boolean> task) {
+                if (task.isSuccessful()) {
+                    boolean updated = task.getResult();
+                    Log.d(TAG, "Config params updated: " + updated);
+                    String find = rem.getString("url");
+                    Log.d(TAG, "Config params updated: " + find);
+                }else {
+                    Toast.makeText(MainActivity.this, "Fetch failed",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
 }
