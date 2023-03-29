f6cbdb6e7f74944e9c79ff3b432be24dc738465a
Was403 <vladimirovichdanil@bk.ru>
2023-03-29 11:05:44 +0300

    btn, imgbtn, textview output

--- app/src/main/java/com/dk/kursach/AppActivity.java
+++ app/src/main/java/com/dk/kursach/AppActivity.java
@@ -2,10 +2,16 @@ package com.dk.kursach;
 
 import android.os.Bundle;
 
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AppActivity extends AppCompatActivity {
 
+    Button btn_code;
+    ImageButton btn_settings, btn_help;
+    TextView printText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
